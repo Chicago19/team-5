12273c9a550295c3497967919840e79d9f19a654
Nihitha Pinnamaraju <pinnamani@umich.edu>
2019-09-20 21:04:19 -0500

    duplicate merge
    idk man

--- frontend/my-app/src/index.js
+++ frontend/my-app/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 //import App from './App';
 import SignIn from './SignIn';
+import Album from './registrationForm';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<SignIn />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const routing = (
+	<Router>
+		<div>	
+			<Route exact path="/" component={SignIn} />
+			<Route path="/registration" component={Album} />
+		</div>
+	</Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
+
