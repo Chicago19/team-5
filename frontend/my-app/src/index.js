c0a3b47b9c81563fbe6510fdcb55ea88f61a6e7b
Kisan Patel <kpate262@uic.edu>
2019-09-21 07:47:45 -0500

    Edited unecessary comments

--- frontend/my-app/src/index.js
+++ frontend/my-app/src/index.js
@@ -11,13 +11,11 @@ const routing = (
 	<Router>
 		<div>
 			<Route exact path="/" component={SignIn} />
-<<<<<<< HEAD
+
 			<Route path="/registration" component={Album} />
-=======
 			<Route path="/registration" component={Registration} />
 			<Route path="/venturestest" component={VenturesTest} />
       <Route path="/profilePage" component={profilePage} />
->>>>>>> 8ec741285e62e149375ccfcc1053aaa4109e8ed1
 		</div>
 	</Router>
 )
