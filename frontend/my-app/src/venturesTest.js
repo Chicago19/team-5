e7124cb4409775f8d462360d3b4d45b11591514d
Peter Zhao <pzhao1799@gmail.com>
2019-09-20 23:43:08 -0500

    prelim ventures test

--- frontend/my-app/src/venturesTest.js
+++ frontend/my-app/src/venturesTest.js
@@ -3,8 +3,15 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
+import RadioButtonCheckedIcon from '@material-ui/icons/RadioButtonChecked';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -27,32 +34,114 @@ function Copyright() {
     );
   }
 
-const useStyles = makeStyles(theme => ({
+  const useStyles = makeStyles(theme => ({
     '@global': {
       body: {
         backgroundColor: theme.palette.common.white,
       },
     },
-    paper: {
+    title: {
         marginTop: theme.spacing(8),
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
       },
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+    formControl: {
+        margin: theme.spacing(3),
+    },
   }));
-  
-  export default function VenturesTest() {
+
+  function RadioButtonsGroup(props) {
     const classes = useStyles();
+    const [value, setValue] = React.useState('female');
+
+  
+    function handleChange(event) {
+      setValue(event.target.value);
+    }
   
     return (
-        <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.newUser}
-        >
-        Ventures Test
-        </Button>
+      <div>
+        <FormControl component="fieldset" className={classes.formControl}>
+          <FormLabel component="legend"> {props.question} </FormLabel>
+          <RadioGroup aria-label="Question" name="Question1" value={value} onChange={handleChange}>
+            <FormControlLabel value="1" control={<Radio />} label= {props.a} />
+            <FormControlLabel value="2" control={<Radio />} label= {props.b} />
+            <FormControlLabel value="3" control={<Radio />} label= {props.c} />
+            <FormControlLabel value="4" control={<Radio />} label= {props.d} />
+          </RadioGroup>
+        </FormControl>
+      </div>
     );
+  }
+
+  export default function VenturesTest() {
+    const classes = useStyles();
+    return (
+        <Container component="main" maxWidth="xs">
+          <Typography variant="h4" className={classes.title}>
+            Ventures Test
+          </Typography>
+          <CssBaseline />
+          <div className={classes.paper}>
+           <img className="logo"
+            src={require("./party.png")} 
+            style={{width: 420, height: 240, margin: 10}}
+            />
+            <form className={classes.form} noValidate>
+            <RadioButtonsGroup 
+            question="1. What's the mother's name?" 
+            a = "His name is Pilar." 
+            b = "Her name is Pilar." 
+            c = "Their name is Pilar."
+            d = "Your name is Pilar."
+            />
+
+            <RadioButtonsGroup 
+            question="2. There are three __ on the table." 
+            a = "cup"
+            b = "cups"
+            c = "cake"
+            d = "plate"
+            />
+
+            <RadioButtonsGroup 
+            question="3. Is Ramona happy?" 
+            a = "Yes, she is"
+            b = "Yes, we are."
+            c = "Yes, he is."
+            d = "Yes, they are."
+            />
+
+            <RadioButtonsGroup 
+            question="2. There are three __ on the table." 
+            a = "cup"
+            b = "cups"
+            c = "cake"
+            d = "plate"
+            />
+            </form>
+          </div>
+          <Box mt={8}>
+            <Copyright />
+          </Box>
+        </Container>
+      );
   }
